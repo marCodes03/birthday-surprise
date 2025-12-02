e1d547e98bcde4cba08144e415e3e9da8b44512a
Marco Amira <marco.amira@eclectustech.net>
2025-12-02 23:37:45 +0800

    (feature) add letter message

--- src/pages/Letter.js
+++ src/pages/Letter.js
@@ -28,11 +28,16 @@ export default function Letter() {
 
 
         <div className={`paper ${opened ? "show" : ""}`} onClick={handleToggle}>
-          <h2>My love, Hina, </h2>
+          <h2>My love, Tsuki, </h2>
           <p>
-            Today is special because you are special. I wrote this letter to remind you
-            how much you mean to me. Every moment with you is a treasure.
+            Hello po, love love! Advance happy birthday sa 'yo, love. Everyday, I am thankful na dumating ka sa buhay ko, love love. You are the love that is truly unexpected na nasasabi ko lagi sa araw-araw na ang swerte ko na dumating ka sa buhay ko. Kaya ngayon na malapit na 'yung kaarawan mo, I want to make you feel special in the best of my abilities. I promise that I won't change. I will love you and be loyal, be faithful, and always be honest sa 'yo, love love.
           </p>
+          <p>
+            Love, I wish para sa 'yo, you remain in love sa akin. I wish na bigyan ka ni God ng madami pang blessings kasi sobrang mapagbigay mong tao and sobrang bait ng tao. I wish na next year, love love, ay papasa ka ng CPALE at magiging basic 'yung review mo, love love. And of course, dito lang po ako, kasama mo po ako all throughout your journey. I also wish for your good health, my love, na huwag ka pong magkakasakit at you always stay healthy and glowing.
+          </p>
+          <p>Advance happy birthday, my love, Tsuki. May all of your heart desires come true, my love. And always remember that I love you so very very much, more than you could ever think of, heart and soul, my love, Miss Christine Joy Amaro.</p>
+          <p>Forever and always yours,</p>
+          <p>Your Hina</p>
         </div>
       </div>
     </div>
